e7ae1a840721c3f2592fea60e6ec77a870ef581e
N. Taylor Mullen <nimullen@microsoft.com>
2019-04-03 09:34:56 -0700

    Remove unneeded using.

--- src/Mvc/Mvc.Razor.RuntimeCompilation/test/FileProviderRazorProjectFileSystemTest.cs
+++ src/Mvc/Mvc.Razor.RuntimeCompilation/test/FileProviderRazorProjectFileSystemTest.cs
@@ -4,7 +4,6 @@
 using System.IO;
 using System.Linq;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Options;
 using Moq;
