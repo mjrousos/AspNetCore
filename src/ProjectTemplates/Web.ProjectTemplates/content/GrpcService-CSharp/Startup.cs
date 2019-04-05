a61a01e4b6fc3014d78d983f09aa043647866711
James Newton-King <james@newtonking.com>
2019-04-05 13:41:51 -0700

    Update src/ProjectTemplates/Web.ProjectTemplates/content/GrpcService-CSharp/Startup.cs
    Co-Authored-By: JunTaoLuo <johluo@microsoft.com>

--- src/ProjectTemplates/Web.ProjectTemplates/content/GrpcService-CSharp/Startup.cs
+++ src/ProjectTemplates/Web.ProjectTemplates/content/GrpcService-CSharp/Startup.cs
@@ -31,7 +31,7 @@ namespace GrpcService_CSharp
             app.UseEndpoints(endpoints =>
             {
                 // Communication with gRPC endpoints must be made through a gRPC client.
-                // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909.
+                // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
                 endpoints.MapGrpcService<GreeterService>();
             });
         }
